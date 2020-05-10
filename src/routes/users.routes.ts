f86092d87bd3fa5824bc274943f68582411bd6b7
David <david_borelli@live.com>
2020-05-10 02:17:08 -0300

    Criptografia de senha

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,6 +11,7 @@ usersRouter.post('/', async (request, response) => {
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({ name, email, password });
+    delete user.password;
 
     return response.json(user);
   } catch (error) {
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -22,10 +23,12 @@ class CreateUserService {
       throw new Error('There is already user with that e-mail');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const createdUser = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(createdUser);
