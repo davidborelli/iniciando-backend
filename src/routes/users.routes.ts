a3abe73ab794e977f807a0328dce9a9192f644a6
David <david_borelli@live.com>
2020-05-10 16:31:59 -0300

    Upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Router, request, response } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthentication';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -18,5 +22,15 @@ usersRouter.post('/', async (request, response) => {
     response.status(400).json({ message: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ message: 'ok' });
+  },
+);
 
 export default usersRouter;
